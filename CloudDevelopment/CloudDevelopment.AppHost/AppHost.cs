2a1f844947c60c2253d5e29e6eda95b77d6057c4
egors <egors@DESKTOP-LA93FBK>
2026-04-29 11:49:51 +0300

    check

--- CloudDevelopment/CloudDevelopment.AppHost/AppHost.cs
+++ CloudDevelopment/CloudDevelopment.AppHost/AppHost.cs
@@ -3,7 +3,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var redis = builder.AddRedis("redis");
 
-
+//check
 builder.AddProject<Projects.GenerationService>("generation-service")
     .WithReference(redis)      
     .WaitFor(redis);            
